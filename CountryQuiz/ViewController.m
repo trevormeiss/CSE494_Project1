12cec85307cf79335c40b92d35eefdc82abf599e
mkarlsrud <mkarlsru@asu.edu>
2014-10-22 12:19:52 -0700

    Initial Commit

---
+++ CountryQuiz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CountryQuiz
+//
+//  Created by mkarlsru on 10/22/14.
+//  Copyright (c) 2014 mkarlsru. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
